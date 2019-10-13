65742668dcde1329c7437e8c77bff3cb96e5d462
Dennis Snell <dennis.snell@automattic.com>
2019-10-12 23:51:57 -0700

    Add Simperium provider for notes
    Before running make sure to replace the APP_ID and API_TOKEN with
    values you get from your Simperium.com dashboard!
    
    In this patch we're connecting to a new Simperium app and using a
    bucket entity to store the contents of the block editor. This is a
    list of block nodes.
    
    Loading of the app takes place after conncting to the bucket and may
    be funny on the first load. If that's the case you might need to
    manually create the new entity...
    
    ```
    bucket.update( 'blocks', { blocks: [] } );
    ```
    
    Afterwards you should be able to connect multiple devices from this
    branch in different browsers or even on different machines and the
    editor contents should stay consistent.
    
    One issue that makes for a buggy experience is that the Simperium
    library is replacing the `blocks` data instead of diffing and merging
    it. This means that one edit session carries a risk of wiping out the
    changes from another session, and if both are offline when they make
    their changes then only one's changes will come though. This should
    be fixable but I didn't have time in this patch to resolve it.

--- src/editor.js
+++ src/editor.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+window.setImmediate = f => setTimeout( f, 0 );
+import React, { useEffect, useState } from "react";
+import Simperium from 'simperium';
 
 /**
  * Gutenberg dependencies:
@@ -21,15 +23,74 @@ import '@wordpress/block-library/build-style/style.css';
 import '@wordpress/block-library/build-style/editor.css';
 import '@wordpress/block-library/build-style/theme.css';
 
+class Ghost {
+	constructor( bucket ) {
+		this.bucket = bucket;
+		this.entries = new Map();
+	}
+
+	getChangeVersion() {
+		return Promise.resolve( this.cv );
+	}
+
+	setChangeVersion( cv ) {
+		this.cv = cv;
+
+		return Promise.resolve( cv );
+	}
+
+	get( id ) {
+		if ( ! this.entries.has( id ) ) {
+			return Promise.resolve( { key: id, data: { blocks: [] } } );
+		}
+
+		return Promise.resolve( this.entries.get( id ) );
+	}
+
+	put( id, version, data ) {
+		this.entries.set( id, { version, data } );
+		return Promise.resolve( true );
+	}
+
+	remove( id ) {
+		this.entries.delete( id );
+		return Promise.resolve();
+	}
+}
 
 const Editor = () => {
+	const [ bucket, setBucket ] = useState( null );
+	const [ loaded, setLoaded ] = useState( false );
 	const [ blocks, updateBlocks ] = useState( [] );
+
+	useEffect( () => {
+		const bucket = Simperium(
+			YOUR_APP_ID_FOR_THIS_DEMO,
+			YOUR_API_TOKEN,
+			{ ghostStoreProvider: bucket => new Ghost( bucket ) }
+		).bucket( 'docs' );
+		setBucket( bucket );
+
+		bucket.on( 'update', ( id, { blocks } ) => {
+			! loaded && setLoaded( true );
+			if ( ! Array.isArray( blocks ) ) {
+				bucket.update( 'blocks', { blocks: [] } );
+			} else {
+				updateBlocks( blocks );
+			}
+		} );
+	}, [] );
+
+	if ( ! loaded ) {
+		return <div>Loading…</div>
+	}
+
 	return (
 		<div id='editor'>
 			<BlockEditorProvider
 				value={ blocks }
-				onInput={ updateBlocks }
-				onChange={ updateBlocks }
+				onInput={ blocks => bucket.update( 'blocks', { blocks } ) }
+				onChange={ blocks => bucket.update( 'blocks', { blocks } ) }
 			>
 				<WritingFlow>
 					<ObserveTyping>
